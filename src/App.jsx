c95c89486469476a85203ebb3f74ce87abbe42c2
RattaphoomT <rattaphoom.t@kkumail.com>
2025-12-03 14:07:48 +0700

    add icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BsTreeFill } from "react-icons/bs";
 import './App.css'
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>- ระบบจัดการร้านขายต้นไม้ -</h1>
+      <h1>- <span><BsTreeFill /></span> ระบบจัดการร้านขายต้นไม้ -</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
